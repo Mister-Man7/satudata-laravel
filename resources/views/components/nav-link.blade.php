06f77312f0a02cf4e5accade86e748d72a0bc0a1
Mister-Man7 <muhamadamanudin6@gmail.com>
2026-06-23 14:15:10 +0700

    initial commit

---
+++ resources/views/components/nav-link.blade.php
@@ -0,0 +1,13 @@
+@props(['active' => false, 'mobile' => false])
+
+<a
+    {{ $attributes->class([
+        'rounded-md px-3 py-2 font-medium',
+        'block w-full text-base' => $mobile,
+        'text-sm' => ! $mobile,
+        'bg-gray-900 text-white' => $active,
+        'text-gray-300 hover:bg-white/5 hover:text-white' => ! $active,
+    ]) }}
+    @if ($active) aria-current="page" @endif
+>{{ $slot }}
+</a>
